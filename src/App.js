e0a3f1617e6bc7a0e3cc664cde8f25c32c423bfe
Hayden Anderson <anderson.hayden@gmail.com>
2021-11-16 17:05:33 -0500

    mobile update 1

--- src/App.js
+++ src/App.js
@@ -1,81 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import * as Tone from 'tone'
+import Main from './pages/Main'
 
 function App() {
-  // test
-
-  // end test
-
-  let reverb = new Tone.Freeverb({
-    roomSize: 0.7,
-    dampening: 3000
-  }).toDestination()
-  let delay = new Tone.FeedbackDelay('8n', 0.75).toDestination()
-
-  const synth = new Tone.Synth().chain(delay, reverb)
-
-  function playNote(note) {
-    synth.triggerAttackRelease(`${note}5`, '8n')
-  }
-
-  // hayden addition
-  const poly1 = new Tone.PolySynth(Tone.AMSynth).chain(delay, reverb)
-  poly1.set({ detune: -1200 })
-
-  function playPoly() {
-    poly1.triggerAttackRelease(['C4', 'E4', 'Ab4', 'Eb5'], '8n')
-  }
-
-  const poly2 = new Tone.PolySynth(Tone.AMSynth).chain(delay, reverb)
-  poly2.set({ detune: -1200 })
-
-  // let chord1 = ['C4', 'E4', 'G4', 'B4']
-  let Cm9 = ['C3', 'D4', 'Eb4', 'G4']
-  let Ab9 = ['Ab2', 'C4', 'Eb4', 'Bb4']
-  let Fm9 = ['F2', 'C4', 'Eb4', 'Ab4']
-  let Bbadd9D = ['D3', 'C4', 'F4', 'Bb4']
-
-  let C5 = ['C4', 'G4', 'C3']
-  let F5 = ['F4', 'C3', 'F3']
-
-  let chordList = [Cm9, Ab9, Fm9, Bbadd9D]
-  // let randomChord = chordList[Math.floor(Math.random() * chordList.length)]
-
-  function playPoly1(chord) {
-    poly2.triggerAttackRelease(chord, '8n')
-  }
-
-  function playRandom() {
-    poly1.triggerAttackRelease(
-      chordList[Math.floor(Math.random() * chordList.length)],
-      '8n'
-    )
-  }
-
-  // end addition
-
   return (
-    <div className="App">
-      <h1 className="title">Aquatic Chords</h1>
-      <div className="chord-wrapper">
-        <button className="chord" onClick={() => playPoly1(Cm9)}>
-          Cm9
-        </button>
-        <button className="chord" onClick={() => playPoly1(Ab9)}>
-          Ab9
-        </button>
-        <button className="chord" onClick={() => playPoly1(Fm9)}>
-          Fm9
-        </button>
-        <button className="chord" onClick={() => playPoly1(Bbadd9D)}>
-          Bbadd9D
-        </button>
-        <button className="chord, random" onClick={() => playRandom()}>
-          random
-        </button>
-      </div>
-    </div>
+    <Router className="App">
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
