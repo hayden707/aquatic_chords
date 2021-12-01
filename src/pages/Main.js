293533ed60614ef2a0df0c86bc8a7ed923f0184a
Hayden Anderson <anderson.hayden@gmail.com>
2021-12-01 17:05:43 -0500

    removed comments and old code

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,10 +4,6 @@ import './Main.css'
 import * as Tone from 'tone'
 
 export default function Main() {
-  // test
-
-  // end test
-
   Tone.Master.volume.value = -10
 
   let reverb = new Tone.Freeverb({
@@ -16,34 +12,18 @@ export default function Main() {
   }).toDestination()
   let delay = new Tone.FeedbackDelay('8n', 0.75).toDestination()
 
-  // const synth = new Tone.Synth().chain(delay, reverb)
-
-  // function playNote(note) {
-  //   synth.triggerAttackRelease(`${note}5`, '8n')
-  // }
-
-  // hayden addition
   const poly1 = new Tone.PolySynth(Tone.AMSynth).chain(delay, reverb)
   poly1.set({ detune: -1200 })
-
-  // function playPoly() {
-  //   poly1.triggerAttackRelease(['C4', 'E4', 'Ab4', 'Eb5'], '8n')
-  // }
 
   const poly2 = new Tone.PolySynth(Tone.AMSynth).chain(delay, reverb)
   poly2.set({ detune: -1200 })
 
-  // let chord1 = ['C4', 'E4', 'G4', 'B4']
   let Cm9 = ['C3', 'D4', 'Eb4', 'G4']
   let Ab9 = ['Ab2', 'C4', 'Eb4', 'Bb4']
   let Fm9 = ['F2', 'C4', 'Eb4', 'Ab4']
   let Bbadd9D = ['D3', 'C4', 'F4', 'Bb4']
 
-  // let C5 = ['C4', 'G4', 'C3']
-  // let F5 = ['F4', 'C3', 'F3']
-
   let chordList = [Cm9, Ab9, Fm9, Bbadd9D]
-  // let randomChord = chordList[Math.floor(Math.random() * chordList.length)]
 
   function playPoly1(chord) {
     poly2.triggerAttackRelease(chord, '8n')
@@ -55,8 +35,6 @@ export default function Main() {
       '8n'
     )
   }
-
-  // end addition
 
   return (
     <div className="App">
